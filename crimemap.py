bc0968e59ffc561eb0c942b86aef1b1ed5cd25b1
nattesharan <nattesharan@gmail.com>
2017-05-04 13:02:31 +0530

    Added view for home

--- crimemap.py
+++ crimemap.py
@@ -1,9 +1,29 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,url_for,redirect
 from dbhelper import DBHelper
 app = Flask(__name__)
 DB = DBHelper()
 @app.route('/')
 def home():
-    return '<html><body><h1><strong><i>Hello, this is home</i></strong></h1></body></html>'
+    try:
+        data = DB.get_all_inputs()
+    except Exception as E:
+        print(E)
+        data = None
+    return render_template('home.html',data = data)
+@app.route('/add', methods = ['POST'])
+def add():
+    try:
+        data = request.form['userinput']
+        DB.add_input(data)
+    except Exception as E:
+        print(E)
+    return redirect(url_for('home'))
+@app.route('/clear')
+def clear():
+    try:
+        DB.clear_all()
+    except Exception as E:
+        print(E)
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(port=int('3000'),debug = True)
--- dbhelper.py
+++ dbhelper.py
@@ -6,3 +6,30 @@ class DBHelper:
                                user = dbconfig.db_user,
                                passwd = dbconfig.db_password,
                                db = database)
+    def get_all_inputs(self):
+        connection = self.connect()
+        try:
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                return cursor.fetchall()
+        finally:
+            connection.close()
+    def add_input(self,data):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            connection.commit()
+        finally:
+            connection.close()
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            connection.commit()
+        finally:
+            connection.close()
