84d984dd4a209be1316c64f3d171214dd0f026e7
nattesharan <nattesharan@gmail.com>
2017-05-04 15:09:33 +0530

    Changes to prevent aql_injection_attack

--- dbhelper.py
+++ dbhelper.py
@@ -18,9 +18,9 @@ class DBHelper:
     def add_input(self,data):
         connection = self.connect()
         try:
-            query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES(%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query,data)
             connection.commit()
         finally:
             connection.close()
