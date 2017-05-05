81070ae89ba06e7aa4b38a6f82ddaf07e5f0e9e2
nattesharan <nattesharan@gmail.com>
2017-05-05 11:36:03 +0530

    Got the json data from the backend to display in the front-end

--- crimemap.py
+++ crimemap.py
@@ -5,11 +5,12 @@ DB = DBHelper()
 @app.route('/')
 def home():
     try:
-        data = DB.get_all_inputs()
+        data = DB.get_all_crimes()
+        print(data)
     except Exception as E:
         print(E)
         data = None
-    return render_template('home.html',data = data)
+    return render_template('home.html')
 @app.route('/add', methods = ['POST'])
 def add():
     try:
@@ -24,6 +25,15 @@ def clear():
         DB.clear_all()
     except Exception as E:
         print(E)
+    return redirect(url_for('home'))
+@app.route('/submitcrime',methods = ['POST'])
+def submitcrime():
+    category = request.form['category']
+    date = request.form['date']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    description = request.form['description']
+    DB.add_crime(category, date, latitude, longitude, description)
     return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(port=int('3000'),debug = True)
